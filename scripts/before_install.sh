43734f323f48f5af1a2ae166d7e90032a3049789
immanueljeosherinvj <immanueljeosherin@gmail.com>
2021-07-17 08:02:46 +0530

    changes to before

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,9 @@
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/ .nvm/nvm.sh
+. ~/.nvm/nvm.sh
 nvm install node
+
 
 
 #creating directory
